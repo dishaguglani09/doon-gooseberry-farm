a28da268341fa9255a037f067b6d67c8c641b781
DevanshSingh7060 <devanshsingh7060@gmail.com>
2026-05-21 20:31:31 +0530

    Fixed merge conflicts and updated ecommerce redesign

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router';
 import { useEffect } from 'react';
 import { AnimatePresence, motion } from 'motion/react';
@@ -20,6 +19,7 @@ import { OrderSuccessPage } from './pages/OrderSuccessPage';
 import { FAQPage } from './pages/FAQPage';
 import { AnnouncementBar } from './components/layout/AnnouncementBar';
 import { WhatsAppButton } from './components/ui/WhatsAppButton';
+import CustomCursor from './components/ui/CustomCursor';
 import { SmoothScroll } from './components/animations/SmoothScroll';
 
 function AnimatedRoutes() {
@@ -63,6 +63,7 @@ function AnimatedRoutes() {
 export default function App() {
   return (
     <BrowserRouter>
+      <CustomCursor />
       <SmoothScroll />
       <div className="relative min-h-screen bg-[#f5f0e8]">
         <AnnouncementBar />
@@ -76,95 +77,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-=======
-import { useState } from "react";
-import HeroSection from "./components/HeroSection";
-import TrustBar from "./components/TrustBar";
-import FeaturedProducts from "./components/home/FeaturedProducts";
-import EditorialQuote from "./components/home/EditorialQuote";
-import IngredientShowcase from "./components/home/IngredientShowcase";
-import SignatureProduct from "./components/home/SignatureProduct";
-import SeasonalHarvest from "./components/home/SeasonalHarvest";
-import ProcessTransparency from "./components/home/ProcessTransparency";
-import WellnessBundles from "./components/home/WellnessBundles";
-import WhyChooseUs from "./components/WhyChooseUs";
-import FounderStory from "./components/home/FounderStory";
-import GiftBoxes from "./components/home/GiftBoxes";
-import GiftGuide from "./components/home/GiftGuide";
-import ShopByCategory from "./components/ShopByCategory";
-import Sustainability from "./components/home/Sustainability";
-import SocialProof from "./components/home/SocialProof";
-import OurStory from "./components/OurStory";
-import InstagramGallery from "./components/InstagramGallery";
-import Newsletter from "./components/Newsletter";
-import PreFooterCTA from "./components/footer/PreFooterCTA";
-import NewFooter from "./components/footer/NewFooter";
-import TrustStrip from "./components/footer/TrustStrip";
-import BackToTop from "./components/footer/BackToTop";
-import LiveChat from "./components/footer/LiveChat";
-import WhatsAppButton from "./components/ui/WhatsAppButton";
-import CustomCursor from "./components/ui/CustomCursor";
-import ShopPage from "./components/shop/ShopPage";
-import AboutPage from "./components/about/AboutPage";
-import AnnouncementBar from "./components/header/AnnouncementBar";
-import Header from "./components/header/Header";
-
-export default function App() {
-  const [currentPage, setCurrentPage] = useState<"home" | "shop" | "about">("home");
-
-  return (
-    <div className="min-h-screen">
-      <CustomCursor />
-      <AnnouncementBar />
-      <Header currentPage={currentPage} onNavigate={setCurrentPage} />
-
-      <div>
-        {currentPage === "home" ? (
-          <div>
-            <HeroSection onShopNowClick={() => setCurrentPage("shop")} />
-            <TrustBar />
-            <FeaturedProducts />
-            <EditorialQuote />
-            <IngredientShowcase />
-            <SignatureProduct />
-            <SeasonalHarvest />
-            <ProcessTransparency />
-            <WellnessBundles />
-            <WhyChooseUs />
-            <FounderStory />
-            <GiftBoxes />
-            <GiftGuide />
-            <ShopByCategory />
-            <Sustainability />
-            <SocialProof />
-            <OurStory />
-            <InstagramGallery />
-            <Newsletter />
-            <PreFooterCTA onShopNowClick={() => setCurrentPage("shop")} />
-            <TrustStrip />
-            <NewFooter />
-          </div>
-        ) : currentPage === "about" ? (
-          <div>
-            <AboutPage onShopNowClick={() => setCurrentPage("shop")} />
-            <PreFooterCTA onShopNowClick={() => setCurrentPage("shop")} />
-            <TrustStrip />
-            <NewFooter />
-          </div>
-        ) : (
-          <div>
-            <ShopPage />
-            <PreFooterCTA onShopNowClick={() => setCurrentPage("shop")} />
-            <TrustStrip />
-            <NewFooter />
-          </div>
-        )}
-      </div>
-
-      <BackToTop />
-      <LiveChat />
-      <WhatsAppButton />
-    </div>
-  );
-}
->>>>>>> c4ad31e53da1c9c7bdaa1bb1e9d4bf7a53f69000
